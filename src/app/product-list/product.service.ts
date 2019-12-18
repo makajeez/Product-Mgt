925935666d459e183d226630e9a804a8f0f30606
Buhari Alhassan Ahmed <50429337+makajeez@users.noreply.github.com>
2019-12-18 16:44:27 +0100

    updts

--- src/app/product-list/product.service.ts
+++ src/app/product-list/product.service.ts
@@ -8,14 +8,13 @@ import { IProduct } from './product';
 @Injectable({
   providedIn: 'root'
 })
-
 export class ProductService {
   private productUrl = 'api/products/products.json';
     constructor(private http: HttpClient) {
     }
-    getProducts(): Observable<IProduct[]> {
-      return this.http.get<IProduct[]>(this.productUrl).pipe(tap(data => console.log('All: ' + JSON.stringify(data))),catchError(this.handleError)
-      );
+    getProducts():Observable<IProduct[]> {
+       return this.http.get<IProduct[]>(this.productUrl).pipe(tap(data => console.log('All: ' + JSON.stringify(data))),catchError(this.handleError)
+       );
     }
     private handleError(err: HttpErrorResponse) {
       let errorMessage = '';
